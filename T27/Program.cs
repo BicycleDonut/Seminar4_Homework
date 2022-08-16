6546070c2de1fe93e846e9107314bb0a2e2b9c99
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-16 21:55:33 +0300

    add T27

---
+++ T27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число:   ");
+int N=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+string strA=N.ToString();
+for (int i = 0; i <strA.Length; i++)
+{
+    sum=sum+(strA[i]-'0');
+}
+Console.WriteLine(sum);
