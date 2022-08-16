49a7b7c09da69e1a3db1264f3cfda36553138e1c
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-16 22:47:37 +0300

    formating tasks

--- T25/Program.cs
+++ T25/Program.cs
@@ -4,16 +4,15 @@
 //2, 4 -> 16
 
 Console.WriteLine("Введите число А:  ");
-int a=Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число В:  ");
-int b=Convert.ToInt32(Console.ReadLine());
-Random Rand=new Random();
-int[] num_ved=new int[b];
-int res=1;
-for (int i = 0; i <num_ved.Length; i++)
+int b = Convert.ToInt32(Console.ReadLine());
+Random Rand = new Random();
+int[] num_ved = new int[b];
+int res = 1;
+for (int i = 0; i < num_ved.Length; i++)
 {
-    num_ved[i]=Rand.Next(a,a);
-    res*=num_ved[i];
+    num_ved[i] = Rand.Next(a, a);
+    res *= num_ved[i];
 }
- Console.WriteLine(res);
- 
+Console.WriteLine(res);
--- T27/Program.cs
+++ T27/Program.cs
@@ -4,11 +4,11 @@
 //9012 -> 12
 
 Console.WriteLine("Введите число:   ");
-int N=Convert.ToInt32(Console.ReadLine());
-int sum=0;
-string strA=N.ToString();
-for (int i = 0; i <strA.Length; i++)
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+string strA = N.ToString();
+for (int i = 0; i < strA.Length; i++)
 {
-    sum=sum+(strA[i]-'0');
+    sum = sum + (strA[i] - '0');
 }
 Console.WriteLine(sum);
