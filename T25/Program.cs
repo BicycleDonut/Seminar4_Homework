1371883bbcef310c9a4335208648c367d2da482b
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-16 13:28:12 +0300

    add T25

---
+++ T25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит
+//число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Введите число А:  ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В:  ");
+int b=Convert.ToInt32(Console.ReadLine());
+Random Rand=new Random();
+int[] num_ved=new int[b];
+int res=1;
+for (int i = 0; i <num_ved.Length; i++)
+{
+    num_ved[i]=Rand.Next(a,a);
+    res*=num_ved[i];
+}
+ Console.WriteLine(res);
+ 
